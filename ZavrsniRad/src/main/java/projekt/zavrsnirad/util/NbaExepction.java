aa41153f1bca9eb9b3c81259093c6a6749a6352f
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-09-19 14:58:00 +0200

    Kreiran nba exepction

---
+++ ZavrsniRad/src/main/java/projekt/zavrsnirad/util/NbaExepction.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt.zavrsnirad.util;
+
+/**
+ *
+ * @author valagic
+ */
+public class NbaExepction {
+    
+    private String poruka;
+    
+    public NbaExepction(String poruka){
+        super();
+        this.poruka = poruka;
+    }
+
+    public String getPoruka() {
+        return poruka;
+    }
+    
+}
